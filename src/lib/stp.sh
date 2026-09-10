873499720fb239ef06f88d7d171c282fc9e707f7
goomba <contactsimpleadress@gmail.com>
2026-09-10 22:26:04 +0100

    Expanded upon the setup.

--- src/lib/stp.sh
+++ src/lib/stp.sh
@@ -1,7 +1,6 @@
 cd ../..
-printf "The current directory is : ["
+printf "The current directory is : "
 pwd
-printf "]\n"
 read -p "Enter target path [default: env/] [type cancel to cancel] : " tar_path
 tar_path="${tar_path:-env/}"
 if [ "$tar_path" = "cancel" ]; then
@@ -11,3 +10,31 @@ fi
 if [ ! -d "$tar_path" ]; then
     mkdir -p "$tar_path"
 fi
+
+shopt -s nullglob
+items=("$tar_path*")
+shopt -u nullglob
+if ! [ "${#items[@]}" -eq 0 ]; then
+    read -p "The directory you provided is not empty, the script will proceed to update the necessary files and folders accordingly (y/n) [default : no] : " update_choice
+    update_choice="${update_choice:-n}"
+    # To lower
+    update_choice="${update_choice,,}"
+    if [ "$update_choice" = "n" ]; then
+        echo "Cannot proceed without confirmation or access to the files inside target directory."
+        exit 1
+    fi
+    echo "The existing directories will be updated accordingly."
+fi
+
+cd "$tar_path"
+
+main_dirs=("backups" "servers" "temp" "data")
+for fd in "${main_dirs[@]}"; do
+    [ ! -d "$fd" ] && mkdir "$fd"
+done
+
+cd "servers"
+server_types=("vanilla" "fabric" "forge")
+for fd in "${server_types[@]}"; do
+    [ ! -d "$fd" ] && mkdir "$fd"
+done
