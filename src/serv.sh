c593919f5c486fac9a23d1b5134035d1b7315fa8
goomba <contactsimpleadress@gmail.com>
2026-09-10 20:27:43 +0100

    Test works, The fs.sh works.

--- src/serv.sh
+++ src/serv.sh
@@ -8,9 +8,3 @@ SERVERS_CREATED_FILE='data/servers.dat'
 
 # Argument control
 
-folder_exists "/dev/shm/server_states/"
-folder_exists "$SERVERS_LOCAL_DATA"
-file_exists "$SERVERS_CREATED_FILE"
-file_exists "$SERVERS_STATE_FILE"
-create_folder "$SERVERS_LOCAL_DATA"
-create_file ""$SERVERS_CREATED_FILE
