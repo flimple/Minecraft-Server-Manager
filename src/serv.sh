2ac7f44a1fb2bd137e097c5f25cc320287aa753d
goomba <contactsimpleadress@gmail.com>
2026-09-10 20:25:55 +0100

    Another quick fix to test.

--- src/serv.sh
+++ src/serv.sh
@@ -3,8 +3,8 @@ source lib/init.sh
 
 # State saving control (Saved for later)
 SERVERS_STATE_FILE='/dev/shm/mc_server_states/global.dat'
-SERVERS_LOCAL_DATA='/data/'
-SERVERS_CREATED_FILE='/data/servers.dat'
+SERVERS_LOCAL_DATA='data/'
+SERVERS_CREATED_FILE='data/servers.dat'
 
 # Argument control
 
