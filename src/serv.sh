7dbc257abf758764aa062656a2e2ba9a212a45bf
goomba <contactsimpleadress@gmail.com>
2026-09-10 21:04:15 +0100

    Integrate fs.sh into the data checks.

--- src/serv.sh
+++ src/serv.sh
@@ -5,6 +5,8 @@ source lib/init.sh
 SERVERS_STATE_FILE='/dev/shm/mc_server_states/global.dat'
 SERVERS_LOCAL_DATA='data/'
 SERVERS_CREATED_FILE='data/servers.dat'
-
-# Argument control
+if ! file_exists "$SERVERS_CREATED_FILE"; then
+    create_folder "$SERVERS_LOCAL_DATA"
+    create_file "$SERVERS_CREATED_FILE"
+fi
 
