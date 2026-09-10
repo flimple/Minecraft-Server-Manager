5a740be19bd7ff1169b8869b7d372badfffff5b2
goomba <contactsimpleadress@gmail.com>
2026-09-10 20:24:47 +0100

    Test after fix.

--- src/serv.sh
+++ src/serv.sh
@@ -3,11 +3,14 @@ source lib/init.sh
 
 # State saving control (Saved for later)
 SERVERS_STATE_FILE='/dev/shm/mc_server_states/global.dat'
+SERVERS_LOCAL_DATA='/data/'
 SERVERS_CREATED_FILE='/data/servers.dat'
 
 # Argument control
 
 folder_exists "/dev/shm/server_states/"
+folder_exists "$SERVERS_LOCAL_DATA"
 file_exists "$SERVERS_CREATED_FILE"
 file_exists "$SERVERS_STATE_FILE"
-create_folder "$SERVERS_CREATED_FILE"
+create_folder "$SERVERS_LOCAL_DATA"
+create_file ""$SERVERS_CREATED_FILE
