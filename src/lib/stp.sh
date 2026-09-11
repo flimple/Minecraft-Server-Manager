06d502651fe1f1a2ab19b0bd22284009f1ebc354
goomba <contactsimpleadress@gmail.com>
2026-09-11 17:45:39 +0100

    Debug Attempt #5 ; servers setup bug 01.

--- src/lib/stp.sh
+++ src/lib/stp.sh
@@ -12,10 +12,8 @@ if [ ! -d "$tar_path" ]; then
 fi
 
 shopt -s nullglob
-items=("$tar_path*")
-echo "$items"
+items=("$tar_path/*")
 shopt -u nullglob
-echo "${#items[@]}"
 if ! [ "${#items[@]}" -eq 0 ]; then
     read -p "The directory you provided is not empty, the script will proceed to update the necessary files and folders accordingly (y/n) [default : no] : " update_choice
     update_choice="${update_choice:-n}"
@@ -31,7 +29,6 @@ fi
 # Fetching the config
 CONFIG_FILE='src/lib/server/servers_config.json'
 server_types=$(jq -r 'keys[]' "$CONFIG_FILE")
-echo "$server_types"
 cd "$tar_path"
 
 # Main dirs init
@@ -44,6 +41,5 @@ done
 
 cd "servers"
 for fd in $server_types; do
-    echo "$fd"
     [ ! -d "$fd" ] && mkdir "$fd"
 done
