3de9bfe87e9db028cd3f2b1519312479e1d2fa95
goomba <contactsimpleadress@gmail.com>
2026-09-11 17:47:50 +0100

    Debug Attempt #6 ; servers setup bug 01.

--- src/lib/stp.sh
+++ src/lib/stp.sh
@@ -13,8 +13,9 @@ fi
 
 shopt -s nullglob
 items=("$tar_path/*")
+count="${items[@]}"
 shopt -u nullglob
-if ! [ "${#items[@]}" -eq 0 ]; then
+if ! [ "$count" -eq 0 ]; then
     read -p "The directory you provided is not empty, the script will proceed to update the necessary files and folders accordingly (y/n) [default : no] : " update_choice
     update_choice="${update_choice:-n}"
     # To lower
