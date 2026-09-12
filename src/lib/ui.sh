2bb8c8ec0573bf104e048f0dfbe50ccb8380464b
goomba <contactsimpleadress@gmail.com>
2026-09-12 22:53:45 +0100

    Expand the ui system, Add a config to organize the nav.

--- src/lib/ui.sh
+++ src/lib/ui.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+descriptions="false"
+if [ ! -d "data/" ]; then
+    create_folder "data/"
+    echo "false" > "data/ui_desc.dat"
+elif [ ! -f "data/ui_desc.dat" ]; then
+    echo "false" > "data/ui_desc.dat"
+else
+    descriptions=$(cat "data/ui_desc.dat")
+fi
+declare -r CONFIG_FILE="lib/server/nav_config.json"
 
-display_top() {
+display_title() {
     echo "┌────────────────────────────────────────────────────────┐"
     figlet -f slant "MINECRAFT"
     figlet -f small "Server Management"
@@ -8,13 +18,6 @@ display_top() {
     return 0
 }
 
-display_server_display() {
-    return 0
-}
-
-display_menu_commands() {
-    return 0
-}
 
 declare -A nav_commands
 declare -i navigation_level=0
@@ -23,7 +26,19 @@ analyze_input() {
     echo "${nav_commands["$input"]}"
 }
 
+# Needs to be below the nav_level var
+display_current_level() {
+    mapfile -t pre_load_actions < <(jq -r ".$navigation_level.button_pre_load_actions[]" "$CONFIG_FILE")
+    for action in "${pre_load_actions[@]}"; do
+        $action
+    done
+    return 0
+}
 
+change_nav_level() {
+    local new_level="$1"
+    return 0
+}
 
 launch_navigation() {
     # The first clear is for clearing the terminal completely before even starting the nav
@@ -31,16 +46,17 @@ launch_navigation() {
 
     # Imo having the dict of nav commands be loaded only after the navigation is called is better
     navigation_level=0
-    nav_commands=( ["refresh"]=continue ["exit"]=cancel )
+    nav_commands=( ["refresh"]="continue" ["exit"]="break" ["servers"]="change_level 1" ["backups"]="change_level 2" ["options"]="change_level 3" ["menu"]="change_level 0" )
     declare -r nav_commands
     local input=""
 
     while true; do
-        display_top
+        display_current_level
         read -p "Please enter a command [default : refresh] : " input
         input="${input:-refresh}"
         clear
         command=$(analyze_input "$input")
         $command
     done
+    return 0
 }
