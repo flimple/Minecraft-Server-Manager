c4db3f62028c78e74df450c9d15e3827b4cd4d7b
goomba <contactsimpleadress@gmail.com>
2026-09-12 22:01:47 +0100

    Automate the navigation and input analysis.

--- src/lib/ui.sh
+++ src/lib/ui.sh
@@ -12,24 +12,35 @@ display_server_display() {
     return 0
 }
 
+display_menu_commands() {
 
+}
+
+declare -A nav_commands
+declare -i navigation_level=0
+analyze_input() {
+    local input="$1"
+    echo "${nav_commands["$input"]}"
+}
 
 
 
 launch_navigation() {
     # The first clear is for clearing the terminal completely before even starting the nav
     clear
+
+    # Imo having the dict of nav commands be loaded only after the navigation is called is better
+    navigation_level=0
+    nav_commands=( ["refresh"]=continue ["exit"]=cancel )
+    declare -r nav_commands
+    local input=""
+
     while true; do
         display_top
         read -p "Please enter a command [default : refresh] : " input
         input="${input:-refresh}"
         clear
-        if [[ $input == "refresh" ]]; then
-            continue
-        fi
-
-        if [[ $input == "exit" ]]; then
-            break
-        fi
+        command=$(analyze_input "$input")
+        $command
     done
 }
