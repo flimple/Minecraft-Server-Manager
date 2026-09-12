a07cbda2e74e1d1d21cc15c9cbd933655ced079e
goomba <contactsimpleadress@gmail.com>
2026-09-12 21:03:56 +0100

    Fix init on the stp serv run.

--- src/serv.sh
+++ src/serv.sh
@@ -8,6 +8,8 @@ SERVERS_CREATED_FILE='data/servers.dat'
 if ! file_exists "$SERVERS_CREATED_FILE"; then
     create_folder "$SERVERS_LOCAL_DATA"
     create_file "$SERVERS_CREATED_FILE"
+    # Added so that the init will be clean without any interruptions
+    exit 0
 fi
 
 # Argument management
