420cdfde3c621a15a49900f8e67bd0e7683ed751
goomba <contactsimpleadress@gmail.com>
2026-09-13 18:11:28 +0100

    Fix the bug by the removing the extre negation.

--- src/lib/ui.sh
+++ src/lib/ui.sh
@@ -140,7 +140,6 @@ display_server_creation() {
                     list_path=$(jq -r --arg key "${fill_data_keys[cr_data_fill_level+1]}" '.[$key].list_path' "$CR_CONFIG_FILE")
                     json_pathing=$(jq -r --arg key "${fill_data_keys[cr_data_fill_level+1]}" '.[$key].list_load_json_command' "$CR_CONFIG_FILE")
                     mapfile -t choices < <(jq -r "$json_pathing" "$list_path")
-                    printf "$choices"
                     if [[ " ${choices[@]} " != *" $cr_input "* ]]; then
                         # The item is non existant
                         echo "$(jq -r --arg key "${fill_data_keys[cr_data_fill_level+1]}" '.[$key].fill_ver_fail_msg' "$CR_CONFIG_FILE")"
